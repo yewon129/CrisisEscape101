a07392f85339c9e8ba29b5944b33d536e4c5795f
이창현 <rover48@hanmail.net>
2022-09-20 16:36:34 +0900

    [VR] feat:지하철 화재 테스트 및 오큘러스 에셋으로 캐릭터 움직임 구현
    Former-commit-id: eac2882e9694b5c80e78e91eb35b755778a8834f

---
+++ speech-processing-server/processing/views.py
@@ -0,0 +1,19 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from . import speech_to_text
+from . import text_similarity
+
+@api_view(['POST'])
+def speech_processing(request):
+    text = request.POST.get("text")
+    speech_file = request.FILES["audio"]
+
+    stt_text = speech_to_text.stt(speech_file)
+
+    sentence = (text, stt_text)
+    similarity = text_similarity.ts(sentence)
+
+    if similarity >= 0.6:
+        return Response(data={'message':True})
+    return Response(data={'message':False})
+    
