f05fecff6aeb9c179f6e8ab725410ed659fd51a2
yewon129 <97646746+yewon129@users.noreply.github.com>
2022-09-23 23:20:51 +0900

    [VR] feat:안내판 작성 &  음성 데이터 저장
    Former-commit-id: af301b8426772ea9f09b4e22b890083f2182636a

---
+++ VR/Assets/audioRec.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+
+public class audioRec : MonoBehaviour
+{
+    AudioSource audio;
+
+    public bool useMicrophone;
+    public string selectedDevice;
+
+
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+    public void SaveVoice()
+    {
+        SavWav.Save("Voice1", audio.clip);
+    }
+
+    public void InputVoice()
+    {
+        if (Microphone.devices.Length > 0)
+        {
+            useMicrophone = true;
+            if (useMicrophone)
+            {
+                selectedDevice = Microphone.devices[0].ToString();//디바이스 선택
+                Microphone.Start(selectedDevice, true, 5, AudioSettings.outputSampleRate); //마이크 입력 시작
+
+                Invoke("SaveVoice", 3);
+            }
+        }
+        else
+        {
+            useMicrophone = false;
+        }
+    }
+    public void PlayVoice()
+    {
+        audio.Play();
+    }
+}
