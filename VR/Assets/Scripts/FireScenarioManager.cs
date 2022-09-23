cc0c1df193ac3e8209fb018d540d7d7fb0b5e1e2
rlawlgns0321 <rlawlgns0321@gmail.com>
2022-09-23 12:03:50 +0900

    [VR] Test: 테스트용 상호작용 물체 및 base 스크립트 배치
    Former-commit-id: a664aaf21392faa3a4095caa6b0cd30c3f524cda

---
+++ VR/Assets/Scripts/FireScenarioManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireScenarioManager : MonoBehaviour
+{
+
+    public GameObject[] guideMessages;
+
+    public GameObject object1;
+    public GameObject object2;
+    public GameObject object3;
+
+    void Start()
+    {
+ 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void Event()
+    {
+
+    }
+}
+
