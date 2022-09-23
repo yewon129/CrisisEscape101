c95ed93f81204b38dae61add3f230ae88557a75c
rlawlgns0321 <rlawlgns0321@gmail.com>
2022-09-23 20:09:13 +0900

    [VR] Test: 상호작용 테스트용 오브젝트 배치 및 소화기 에셋 추가
    Former-commit-id: 453f28136bb571c0b90f6e9c6b36a846d3e40ed3

--- VR/Assets/Scripts/FireScenarioManager.cs
+++ VR/Assets/Scripts/FireScenarioManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class FireScenarioManager : MonoBehaviour
 {
@@ -11,9 +12,16 @@ public class FireScenarioManager : MonoBehaviour
     public GameObject object2;
     public GameObject object3;
 
+    public static int stage = 0;
+
     void Start()
     {
- 
+        object1.GetComponent<XRGrabInteractable>().enabled = true;
+        object1.GetComponent<Sequence>().enabled = true;
+        object2.GetComponent<XRGrabInteractable>().enabled = false;
+        object1.GetComponent<Sequence>().enabled = false;
+        object3.GetComponent<XRGrabInteractable>().enabled = false;
+        object1.GetComponent<Sequence>().enabled = false;
     }
 
     // Update is called once per frame
@@ -22,9 +30,5 @@ public class FireScenarioManager : MonoBehaviour
 
     }
 
-    void Event()
-    {
-
-    }
 }
 
