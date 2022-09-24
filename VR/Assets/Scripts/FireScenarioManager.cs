79e16d0506eaf44f0c639ef8517bc9074b98b3db
rlawlgns0321 <rlawlgns0321@gmail.com>
2022-09-25 03:08:10 +0900

    [VR] Feat: 비상보관함 열고 방독면 착용기능 구현
    Former-commit-id: ec824d1d7a2f545446280bc24c0c8aa7fda04aaa

--- VR/Assets/Scripts/FireScenarioManager.cs
+++ VR/Assets/Scripts/FireScenarioManager.cs
@@ -2,32 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
 
 public class FireScenarioManager : MonoBehaviour
 {
 
     public GameObject[] guideMessages;
+    public static FireScenarioManager instance;
 
     public GameObject object1;
     public GameObject object2;
     public GameObject object3;
+    public static Volume GasMask;
 
     public static int stage = 0;
+    public static bool isGasMaskOn = false;
+    public static Vignette vg;
+
+    public static void GasMaskEffect()
+    {
+        vg.intensity.value = 1f;
+    }
 
     void Awake ()
     {
+        instance = this;
+        GasMask = GameObject.Find("PostProcessingGasMask").GetComponent<Volume>();
+        GasMask.profile.TryGet(out vg);
+
+
         object1.GetComponent<XRGrabInteractable>().enabled = true;
         object1.GetComponent<Sequence>().enabled = true;
         object2.GetComponent<XRGrabInteractable>().enabled = false;
         object2.GetComponent<Sequence>().enabled = false;
         object3.GetComponent<XRGrabInteractable>().enabled = false;
         object3.GetComponent<Sequence>().enabled = false;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
     }
 
 }
---
+++ VR/Assets/Scripts/GasMaskCabinetOpener.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class GasMaskCabinetOpener : MonoBehaviour
+{
+
+    public bool isLeft;
+    public Rigidbody rigidBody;
+    XRBaseInteractable m_Interactable;
+    private bool isOpen = false;
+    Vector3 angleVelocity;
+
+    void OnEnable()
+    {
+        m_Interactable = GetComponent<XRBaseInteractable>();
+        m_Interactable.firstSelectEntered.AddListener(OnFirstSelectEntered);
+        rigidBody = GetComponent<Rigidbody>();
+        angleVelocity = new Vector3(0, -180, 0);
+    }
+
+    protected virtual void OnFirstSelectEntered(SelectEnterEventArgs args) => OpenDoor();
+
+
+    void FixedUpdate()
+    {
+        if (isOpen)
+        {
+            StartCoroutine(DoorMove());
+            Quaternion deltaRotation = Quaternion.Euler(angleVelocity * Time.fixedDeltaTime);
+            rigidBody.MoveRotation(rigidBody.rotation * deltaRotation);
+        }
+    }
+
+    void OpenDoor()
+    {
+        isOpen = true;
+    }
+
+    IEnumerator DoorMove()
+    { 
+        
+        yield return new WaitForSeconds(1f);
+        isOpen = false;
+
+    }
+}
