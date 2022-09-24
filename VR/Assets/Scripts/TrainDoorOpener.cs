a83eaf527a1951472df27693f43886af45b3d7de
rlawlgns0321 <rlawlgns0321@gmail.com>
2022-09-24 23:30:41 +0900

    [VR] Feat: 지하철 문 개방기능 추가
    Former-commit-id: b314aa7c9546684dcceb089474760b63a00187c1

---
+++ VR/Assets/Scripts/TrainDoorOpener.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class TrainDoorOpener : MonoBehaviour
+{
+
+    public bool isLeft;
+    public Rigidbody rigidBody;
+    XRBaseInteractable m_Interactable;
+
+    void OnEnable()
+    {
+        m_Interactable = GetComponent<XRBaseInteractable>();
+        m_Interactable.firstSelectEntered.AddListener(OnFirstSelectEntered);
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    protected virtual void OnFirstSelectEntered(SelectEnterEventArgs args) => OpenDoor();
+
+    void OpenDoor()
+    {
+        StartCoroutine(DoorMove());
+    }
+
+    IEnumerator DoorMove()
+    {
+        Debug.Log("act!");
+        rigidBody.velocity = new Vector3(0, 0, 0.5f);
+        if (isLeft)
+            rigidBody.velocity *= -1;
+        
+        yield return new WaitForSeconds(0.6f);
+
+        rigidBody.velocity = new Vector3(0, 0, 0);
+
+    }
+}
