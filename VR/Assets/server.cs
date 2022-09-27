c86dc40e493394063e567f781d14a3db74a242bd
yewon129 <97646746+yewon129@users.noreply.github.com>
2022-09-27 13:51:43 +0900

    [VR] fix: http request
    Former-commit-id: a21024d922523aeb2bd38c713398de84ac1b5a9b

--- VR/Assets/server.cs
+++ VR/Assets/server.cs
@@ -10,7 +10,7 @@ public class server : MonoBehaviour
 {
     public string STTtext;
     public byte[] STTaudio;
-
+    public AudioClip STTclip;
     public void Ready()
     {
         Debug.Log("STTaudio is " + STTaudio);   
@@ -19,18 +19,20 @@ public class server : MonoBehaviour
     }
 
     IEnumerator Upload(string stttext, byte[] sttaudio)
-    {   
+    {
+
 
         List<IMultipartFormSection> formData = new List<IMultipartFormSection>();
 
         Debug.Log("stttext is " + stttext);
         Debug.Log("sttaudio is " + sttaudio);
 
+
         formData.Add(new MultipartFormDataSection("text", stttext));
         formData.Add(new MultipartFormFileSection("audio", sttaudio));
 
         Debug.Log("formData");
-        Debug.Log(formData);
+        Debug.Log(formData[1]);
 
         UnityWebRequest www = UnityWebRequest.Post("http://j7a101.p.ssafy.io:8080/api/v1/processing/", formData);
         www.SetRequestHeader("Content-Type", "multipart/form-data; boundary=<calculated when request is sent>");
