a7a1b9b01815984f8325f8a1952d8af8121d810c
yewon129 <97646746+yewon129@users.noreply.github.com>
2022-09-29 18:01:32 +0900

    [VR] feat: 안내창 버튼 없이 진행
    Former-commit-id: d98e2dae1e56a0373e03c1c6295850fe906ce422

---
+++ VR/Assets/BtnSetActive.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BtnSetActive : MonoBehaviour
+{
+    public GameObject TargetBg;
+
+    public void TargetSetActive()
+    {
+        TargetBg.SetActive(!TargetBg.active);
+    }
+}
--- VR/Assets/CloseCanvas.cs
+++ VR/Assets/CloseCanvas.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class CloseCanvas : MonoBehaviour
 {
     public GameObject item;
+    public float delay;
 
     // Start is called before the first frame update
     void Start()
     {
-        Invoke("Close", 7f);
+        Invoke("Close", delay);
     }
 
     // Update is called once per frame
