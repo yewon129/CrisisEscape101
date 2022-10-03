e5626c5942ccbf5e7bc6024883efda4c2be17322
김용환 <ymin96@naver.com>
2022-10-04 04:31:51 +0900

    [VR] feat: 비상개폐장치 돌려야 문 열리도록 수정
    Former-commit-id: ffe9bd82004e6bfaec405045411cc191054f9374

---
+++ VR/Assets/ActiveDoor.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+public class ActiveDoor : MonoBehaviour
+{
+    public GameObject leftDoor;
+    public GameObject rightDoor;
+    XRGrabInteractable leftXr;
+    XRGrabInteractable rightXr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        leftXr = leftDoor.GetComponent<XRGrabInteractable>();
+        rightXr = rightDoor.GetComponent<XRGrabInteractable>();
+        Debug.Log("비상벨브");
+    }
+
+    public void activeDoor()
+    {
+        leftXr.enabled = true;
+        rightXr.enabled = true;
+        Debug.Log("비상벨브 잡기");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
