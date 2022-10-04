2407bc36dbff28dcb1446c3b7723939b52c7e4c7
yewon129 <97646746+yewon129@users.noreply.github.com>
2022-10-04 14:31:14 +0900

    [VR] feat: 메인페이지 작성
    Former-commit-id: 7b5882dbf80888ab9dd6e1b8ed6415b383e0e62e

---
+++ VR/Assets/Voice/ClickMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ClickMenu : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    public void ChangeFireScene()
+    {
+        Debug.Log("fire");
+        SceneManager.LoadScene("text-mesh");
+    }
+    public void ChangeWaterScene()
+    {
+        Debug.Log("fire");
+        SceneManager.LoadScene("WorldInteractionDemo");
+    }
+    public void QuitScene()
+    {
+        Application.Quit();
+    }
+
+}
